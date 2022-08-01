1d2d5ad4c15505c7aed3e99f2c2f54d44083896f
Lancet <rainbean@126.com>
2022-08-01 20:53:44 +0800

    add firebase SDK
    Took 52 minutes

--- app/src/dev/java/cn.lancetmusic/DevLancetApp.kt
+++ app/src/dev/java/cn.lancetmusic/DevLancetApp.kt
@@ -8,7 +8,7 @@ import io.realm.Realm
 /**
  * Created by Lancet at 2022/4/30 22:42
  */
-open class LancetApp : Application() {
+open class DevLancetApp : LancetApp() {
 
     override fun onCreate() {
         super.onCreate()
